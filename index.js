db88364a1b7cd14d86c3bc23e2ecd18991de075d
Aucante <sebastien.aucante@gmail.com>
2022-08-28 12:31:08 +0200

    exercice 1 section 7

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-(function(){ console.log('Je suis une fonction anonyme')})();
+function abracadbara() {
+    let nom = prompt('Quel est votre nom ?');
+    let prenom = prompt('Quel est votre prenom ?');
+    let age = prompt('Quel est votre age ?');
+    alert("Sapristi ! On ne m'avait pas prévenu que c'était vous, " + prenom + " ! Euh... Je veux dire... Monsieur le grand magicien " + nom + " ! Cela fait déjà " + age + " ans que vous faites rayonner notre contrée !")
+}
+
+abracadbara();
