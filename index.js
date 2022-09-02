e8d30c48af7c53d5766bca647ea2f2c78f761300
Aucante <sebastien.aucante@gmail.com>
2022-09-02 14:35:56 +0200

    concaténation

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-let pays = ['France', 'Belgique', 'Japon', 'Espagne'];
+let monTableauAssociatif = {
+    'prenom'    : 'Mark',
+    'nom'       : 'Zuckerberg',
+    'poste'     : 'PDG de Facebook'
+};
 
-pays.forEach(key => console.log(key));
+function concatener(tableau) {
+    let chaine = '';
+
+    for (const valeur in tableau) {
+        
+        chaine += (valeur + ' : ' + tableau[valeur] + '\n');
+
+    }
+
+    console.log(chaine);
+}
+
+concatener(monTableauAssociatif);
