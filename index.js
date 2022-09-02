fd3576d5f485e050a826a3957babd9b36399cb3b
Aucante <sebastien.aucante@gmail.com>
2022-09-02 14:07:34 +0200

    pop and shift

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 let monTableau = ['un', 'deux', 'trois', 'quatre'];
-console.log(monTableau);
-monTableau.push('cinq');
-console.log(monTableau);
-monTableau.unshift('zero');
-console.log(monTableau);
+monTableau.pop(); // supprime le dernier élément
+monTableau.shift(); // supprime le premier élément
 // console.log(monTableau);
 
 let monTableau2D = [
     ['Mark', 'Jeff', 'Bill'],
     ['Zuckerberg', 'Bezos', 'Gates']
 ];
-monTableau2D[1].unshift('test');
+monTableau2D[0].shift(); // supprime le premier élément, de mon premier élément (donc, Mark)
 // console.log(monTableau2D);
 
 let monTableauAssociatif = {
@@ -18,6 +15,5 @@ let monTableauAssociatif = {
     'nom'    : 'Zuckerberg',
     'poste'  : 'PDG de Facebook'
 };
-
-monTableauAssociatif['nationalite'] = 'Américaine';
-console.log(monTableauAssociatif);
+delete(monTableauAssociatif.poste);
+// console.log(monTableauAssociatif);
