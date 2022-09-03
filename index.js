53d4291c138cac9aaf3b3c604dc8f622189bda53
Aucante <sebastien.aucante@gmail.com>
2022-09-03 11:58:21 +0200

    closures

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 
+let secondes = 0;
+function timer() {
+    
+    
+    let maClosure = () => {
+      return ++secondes;
+    }
+    return maClosure;
+  }
   
-  let maFonction = () => console.log('test');
-
-  maFonction();
+  let monTimer = timer();
+  console.log(monTimer());
+  console.log(monTimer());
+  console.log(monTimer());
