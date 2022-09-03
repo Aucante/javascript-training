e41a91d4934f03a5e71e846974a713f5244cc548
Aucante <sebastien.aucante@gmail.com>
2022-09-03 12:11:11 +0200

    decomposition

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-let chien = {
-    race: 'Shiba',
-    poil: 'Court',
-    // aboyer: function(){
-    //     console.log('Ouaf ouaf');
-    // }
-    aboyer: () => console.log('Ouaf ouaf')
-}
+let informations = ['superSayen', '25', 'homme'];
 
-chien.aboyer();
+// Sans décomposition
+// let pseudo = informations[0];
+// let age    = informations[1];
+// let sexe   = informations[2];
+
+// Avec décomposition
+let [pseudo, age, sexe] = informations;
+
+console.log(pseudo);
+console.log(age);
+console.log(sexe);
