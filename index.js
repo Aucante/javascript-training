726fc91a1d76a93d38e39251d51ac2ddc051cac0
Aucante <sebastien.aucante@gmail.com>
2022-09-07 22:15:04 +0200

    weakmap

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+let voitures = new WeakMap();
+
+let index = {
+    id: 1
+}
+
 let voitureA = {
     constructeur: 'Tesla',
     modele: 'Cybertruck'
-  }
-  let voitureB = {
-    constructeur: 'Renault',
-    modele: 'Espace'
-  }
-  
-  let voitures = new WeakSet([voitureA, voitureB]);
-  console.log(voitures);
+}
+
+voitures.set(index, voitureA);
+
+// voitures.delete(index);
+console.log(voitures);
