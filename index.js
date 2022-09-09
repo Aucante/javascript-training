64d219c66ea6b801bd813e3e4a08cf8891568dac
Aucante <sebastien.aucante@gmail.com>
2022-09-09 16:04:29 +0200

    accéder aux elements du DOM

--- index.js
+++ index.js
@@ -1,24 +1,9 @@
-// Exemple 1 : fusionner plusieurs tableaux
-let fruits = ['fraise', 'banane', 'poire'];
-let aliments = ['chocolat', 'sucre', 'lait', ...fruits];
+let header = document.getElementsByTagName('header');
 
-// console.log(aliments);
+let logo = document.getElementById('logo');
 
-// Exemple 2 : découper une chaîne de caractères en plusieurs éléments
-let phrase = "Bonjour !";
-let phraseTableau = [...phrase];
+let container = document.getElementsByClassName('container');
 
-// console.log(phraseTableau);
-
-// Exemple 3 : sélectionner un élément, et stocker les autres dans une variable
-let devises = ['dollars', 'euro', 'yen'];
-
-// Sans décomposition
-// let premiereDevise = devises[0];
-// let autres = [devises[1], devises[2]];
-
-// Avec décomposition
-let [premiereDevise, ...autres] = devises;
-
-console.log(premiereDevise);
-console.log(autres);
+console.log(header);
+console.log(logo);
+console.log(container);
