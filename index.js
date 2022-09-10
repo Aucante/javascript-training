d1ac398d0b2d8aed2a94e7571b715d8ff2cd468d
Aucante <sebastien.aucante@gmail.com>
2022-09-10 17:25:21 +0200

    ecouteurs evenements

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-// Méthode la plus ancienne
-// Déconseillé car vieillissante
-// On insère un attribut HTML lié à l'évènement qu'on veut capturer
-// onClick, onMouseOver, onMouseOut
+let a = document.querySelector('a');
+let button = document.querySelector('button');
+
+// a.onclick = function(){
+//   if(confirm('Etes-vous sûr ?')) {
+//     location.href="https://believemy.com"
+//   }
+// }
+
+a.onclick = () => {
+  if(confirm('Etes-vous sûr ?')) {
+    location.href="https://believemy.com"
+  }
+}
+
+button.onmouseover = () => {
+  document.body.style.backgroundColor = 'orange';
+}
+
+button.onmouseout = () => {
+  document.body.style.backgroundColor = 'white';
+}
