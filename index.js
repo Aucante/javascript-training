d39d26c12ac7f8e0c5e5ad3ded48610fe2bd919e
Aucante <sebastien.aucante@gmail.com>
2022-09-13 21:40:29 +0200

    constructeur

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
-let mark = {
-  prenom: "Mark", // propriété
-  nom: "Zuckerberg",
-  email: "mark@facebook.com",
+// On crée une fonction constructeur pour nos utilisateurs
+function Utilisateur(prenom, nom, email) {
+  this.prenom = prenom;
+  this.nom    = nom;
+  this.email  = email;
   
-  // À l'ancienne
-  sePresenter: function(){
-    console.log("Bonjour, je m'appelle " + this.prenom);
+  this.sePresenter = () => {
+    console.log("Bonjour, je m'appelle " + this.prenom + " " + this.nom + " et vous pouvez me contacter à l'adresse email " + this.email);
   }
 }
 
-mark.sePresenter();
+// On crée un objet
+var mark = new Utilisateur("Mark", "Zuckerberg", "mark@facebook.com");
+var bill = new Utilisateur("Bill", "Gates", "bill@gatesnotes.com");
 
-function recevoirLesCoordonnees() {
-  return { latitude: 35, longitude: 139 }
+// mark.sePresenter();
+// console.log(mark);
+
+mark.poste = "PDG de Facebook";
+
+// console.log(mark);
+// console.log(bill);
+
+function Logement(type, annee, placeDeParking, proprietaire) {
+  this.type             = type;
+  this.annee            = annee;
+  this.placeDeParking   = placeDeParking;
+  this.proprietaire     = proprietaire;
 }
 
-let coordonnees = recevoirLesCoordonnees();
-console.log(coordonnees.latitude);
-console.log(coordonnees.longitude);
+var appartementA = new Logement('Appartement', 2014, true, mark); // Mark est un objet
+
+// console.log(appartementA.proprietaire.prenom);
+console.log(appartementA);
