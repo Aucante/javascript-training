ffdd3f7b1ff308f0bf35f624e7cd6d7838fb77b8
Aucante <sebastien.aucante@gmail.com>
2022-09-17 21:13:54 +0200

    post request

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
-const url = 'https://blockchain.info/ticker';
+const url = 'https://lesoublisdelinfo.com/api.php';
 
-function recupPrix() {
-    let requete = new XMLHttpRequest();
-    requete.open('GET', url);
-    requete.responseType = 'json';
-    requete.send();
+let requete = new XMLHttpRequest();
+requete.open('POST', url);
+requete.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+requete.responseType = 'json';
+requete.send('prenom=John');
 
-    requete.onload = function() {
-        if (requete.readyState === XMLHttpRequest.DONE) {
-            if (requete.status === 200) {
-                let reponse = requete.response;
-                let prixEuros = reponse.EUR.last;
-                document.querySelector('#price_label').textContent = prixEuros;
-            }
-            else
-            {
-                alert("problème");
-            }
+requete.onload = function() {
+    if (requete.readyState === XMLHttpRequest.DONE) {
+        if (requete.status === 200) {
+            let reponse = requete.response;
+            console.log(reponse);
+        }
+        else
+        {
+            alert('problème');
         }
     }
-    console.log('Prix actualisé');
 }
-
-setInterval(recupPrix, 1000);
-
