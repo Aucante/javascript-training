efc2e82b836760a1c09f8126c10c4282b0ef8a1e
Aucante <sebastien.aucante@gmail.com>
2022-09-19 21:41:28 +0200

    promesse

--- index.js
+++ index.js
@@ -1,32 +1,41 @@
-let ville = "Paris";
-recevoirTemperature(ville);
+// Les promesses (async et await)
+// Objet qui représente l'état d'une opération asynchrone
+// En cours / Honorée / Rompue
+// Exemple : Discord(), app de discussion en ligne, permet de parler aussi par micro. Utilise l'asynchrone pour ne pas bloquer tant que vous n'acceptez pas l'utilisation du micro.
 
-let changerVille = document.querySelector('#changer').addEventListener('click', () => {
-    ville = prompt('Quelle ville souhaitez vous?');
-    recevoirTemperature(ville);
-});
+// const promesse = new Promise((resolve, reject) => {
+//   // Tâches asynchrones
+  
+//   // Promesse honorée : appel de resolve()
+  
+//   // Promesse rompue : appel de reject()
+// });
 
-function recevoirTemperature(ville) {
+// Mais généralement, nous ne créerons presque jamais de promesse, car les API modernes qu'on pourrait utiliser les ont déjà créées pour nous. Elles renvoient donc souvent un objet promesse en résultat qu'on utilisera pour savoir si tout s'est bien passé.
 
-    const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + ville + '&appid=dc8c9152e8adaad0ec8bf635818c0d42&units=metric';
-
-    let requete = new XMLHttpRequest();
-    requete.open('GET', url);
-    requete.responseType = 'json';
-    requete.send('prenom=John');
-    
-    requete.onload = function() {
-        if (requete.readyState === XMLHttpRequest.DONE) {
-            if (requete.status === 200) {
-                let reponse = requete.response;
-                document.querySelector('#temperature_label').textContent = reponse.main.temp;
-                document.querySelector('#ville').textContent = reponse.name;
-            }
-            else
-            {
-                alert('problème');
-            }
-        }
-    }
-}
-
+function chargerScript(script) {
+    return new Promise((resolve, reject) => {
+      let element = document.createElement('script');
+      element.src = script;
+      document.head.append(element);
+      element.onload = () => resolve('Fichier ' + script + ' chargé');
+      element.onerror = () => reject(new Error('Operation impossible pour le script ' + script));
+    });
+  }
+  // const promesse = chargerScript('test.js');
+  
+  // // Renvoi une erreur car le fichier test.js n'existe pas
+  // promesse.then(
+  //   function(result) {
+  //     console.log(result);
+  //   },
+  //   function(error) {
+  //     console.log(error);
+  //   }
+  // );
+  
+  // promesse.then(result => console.log(result), error => console.log(error)); // Même code, avec des fonctions fléchées
+  
+  // On peut aussi passer directement par la fonction elle-même
+  chargerScript('test.js')
+  .catch(console.log);
