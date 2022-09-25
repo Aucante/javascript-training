964016295466602a66d90f9acbdebc3088f6933f
Aucante <sebastien.aucante@gmail.com>
2022-09-25 18:02:06 +0200

    geolocaliser user

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
-let bouton = document.querySelector('#mode');
-let span = document.querySelector('span');
-let body = document.querySelector('body');
+// Pour géolocaliser on utilise l'objet geolocation
 
-if (localStorage.getItem('theme')) {
-  if(localStorage.getItem('theme') == 'sombre') {
-    modeSombre();
-  }
-} 
-  bouton.addEventListener('click', () => {
-    if (body.classList.contains('dark')) {
-      body.classList.remove('dark');
-      span.innerHTML = 'Thème sombre';
-      localStorage.setItem('theme', 'clair');
-    } else {
-      modeSombre();
-    }
-  });
+// Vérifier que la geolocation soit disponible
+if ('geolocation' in navigator) {
   
-  function modeSombre() {
-    body.classList.add('dark');
-    span.innerHTML = 'Thème clair';
-    localStorage.setItem('theme', 'sombre');
+  // Obtenir la position avec getCurrentPosition() ou watchPosition()
+  
+  // (success, error (optionnel), options (optionnel))
+  
+  navigator.geolocation.getCurrentPosition((position) => {
+    console.log(position.coords.latitude);
+    console.log(position.coords.longitude);
+  }, error, options);
+  
+  function error() {
+    alert('Aucune position disponible.');
   }
-
-
+  
+  var options = {
+    enableHighAccuracy: true,  // false par defaut
+    maximumAge        : 30000, // derniere position en cache en millisecondes qu'on peut accepter, 0 par defaut
+    timeout           : 27000  // duree max en millisecondes pour geolocaliser, infinity par defaut
+  }
+  
+  /*
+  let watch = navigator.geolocation.watchPosition() ...
+  navigator.geolocation.clearWatch(watch)
+  */
+  
+}
+else {
+  alert('Le navigateur ne supporte pas la geolocalisation');
+}
